9bd8af7ad52d38e7d8e647c6d7968887d3a974f2
Jamie Kohns <jamiekohns@gmail.com>
2025-03-29 15:59:46 +0000

    WIP: rps game code

--- rps.py
+++ rps.py
@@ -4,8 +4,26 @@ import re
 
 # Comment
 def check_play_status():
-    print('Play Status');
+    # valid_responses = ['yes', 'no', 'y']
+    while True:
+        try:
+            response = input('Do you wish to play again? (Yes or No): ')
+            if response.lower() not in ['yes', 'no', 'y']:
+                raise ValueError('Yes or No only')
+
+            if response.lower() in ['yes', 'y']:
+                return True
+            else:
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print('Thanks for playing!')
+                exit()
+
+        except ValueError as err:
+            print(err)
+
 
 def play_rps():
-    print('Playing');
+    play = check_play_status()
+
+play_rps()
 
